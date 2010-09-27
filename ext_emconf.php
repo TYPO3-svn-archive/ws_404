ed628efffdecf597137aa687a5b41d989b0e593e
okolya <okolya@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 07:54:48 +0000

    First upload to SVN
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_404/trunk@38630 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ws_404".
+#
+# Auto generated 23-09-2010 01:43
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '404 Handler',
+	'description' => '404 content page without redirection. Works with multilanguage, multidomain, multi page types(TypeNum)',
+	'category' => 'fe',
+	'author' => 'Nikolay Orlenko',
+	'author_email' => 'okolya@gmail.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"8a1b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"31f8";s:20:"doc/wizard_form.html";s:4:"1e70";}',
+);
+
+?>
