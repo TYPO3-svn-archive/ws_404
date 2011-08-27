a3b13335c2af53d9ed7244e1595f17eac9657fd7
okolya <okolya@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-27 15:08:15 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_404/trunk@51471 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ws_404".
 #
-# Auto generated 27-08-2011 14:47
+# Auto generated 27-08-2011 16:52
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '404 content page without redirection. Works with multilanguage, multidomain, multi page types(TypeNum)',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.3.0',
+	'version' => '0.3.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"c61a";s:10:"README.txt";s:4:"ee2d";s:27:"class.user_pageNotFound.php";s:4:"ecbb";s:21:"ext_conf_template.txt";s:4:"e860";s:12:"ext_icon.gif";s:4:"1b49";s:17:"ext_localconf.php";s:4:"2e7e";s:19:"doc/wizard_form.dat";s:4:"31f8";s:20:"doc/wizard_form.html";s:4:"1e70";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"1e1a";s:10:"README.txt";s:4:"ee2d";s:27:"class.user_pageNotFound.php";s:4:"3c03";s:21:"ext_conf_template.txt";s:4:"e860";s:12:"ext_icon.gif";s:4:"1b49";s:17:"ext_localconf.php";s:4:"2e7e";s:14:"doc/manual.sxw";s:4:"e3fb";}',
+	'suggests' => array(
+	),
 );
 
 ?>
